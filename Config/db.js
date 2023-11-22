e1ed769aaf2386af9584592729621166bc850dd5
Agaam03 <agam.prasetya03@gmail.com>
2023-11-22 21:48:19 +0700

    add product model

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const DB_URL = "mongodb+srv://agaam:agaam@atlascluster.0mt5lxe.mongodb.net/Final_Project" || "mongodb:localhost/myapp";
+const DB_URL = "mongodb+srv://agaam:agaam@atlascluster.0mt5lxe.mongodb.net/Final_Project";
 
 const db = mongoose.connect(DB_URL);
 
---
+++ Models/product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "Please Enter product Name"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please Enter product Description"],
+    },
+    harga: {
+      type: Number,
+      required: [true, "Please Enter product Price"]
+    },
+    images: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please Enter Product Category"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  });
+  
+  module.exports = mongoose.model("Product", productSchema);
