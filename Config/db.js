cf75f041fe9e845c081edc885701dea066f77840
Agaam03 <agam.prasetya03@gmail.com>
2023-12-01 02:30:18 +0700

    s

--- Config/db.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const mongoose = require("mongoose");
-
-const DB_URL = "mongodb+srv://agaam:agaam@atlascluster.0mt5lxe.mongodb.net/Final_Project";
-
-const db = mongoose.connect(DB_URL);
-
-module.exports = db;
